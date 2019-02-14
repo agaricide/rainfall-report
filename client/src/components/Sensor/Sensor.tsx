5d862c346670a693ff0ce604dc657db2864d460a
agaricide <agaricide@gmail.com>
2019-02-14 16:32:13 -0600

    Added Google maps functionality

--- client/src/components/Sensor/Sensor.tsx
+++ client/src/components/Sensor/Sensor.tsx
@@ -3,21 +3,29 @@ import { SensorData, RainfallInterval } from '../../rainfall/types';
 import Interval from './Interval';
 import './Sensor.css';
 
-const intervals: RainfallInterval[] = ['sixHours', 'oneDay', 'sevenDays', 'thirtyDays', 'pastYear'];
-
 const Sensor = (props: SensorData) => {
   const [interval, setInterval] = useState<RainfallInterval>('sevenDays');
+
+  const goToPoint = (latitude: number, longitude: number) => {
+    const url = `https://www.google.com.sa/maps/search/${latitude},${longitude}`;
+    window.open(url, '_blank');
+  };
 
   return (
     <div className='report'>
       <div className='location'>
+        <span onClick={() => goToPoint(props.latitude, props.longitude)} className='point'>&#10005;</span>
         {props.name}
       </div>
       <div className='rainfall'>
-        {props[interval]}<span className='units'> in</span>
+        {props[interval] || '0.0'}<span className='units'> in</span>
       </div>
-      <ul className='intervals'>
-        {intervals.map(i => <Interval interval={i} onClick={setInterval}/>)}
+      <ul className='intervals wide'>
+        <Interval display={'6hr'} interval={'sixHours'} onClick={setInterval} />
+        <Interval display={'24hr'} interval={'oneDay'} onClick={setInterval} />
+        <Interval display={'7d'} interval={'sevenDays'} onClick={setInterval} />
+        <Interval display={'1mo'} interval={'thirtyDays'} onClick={setInterval} />
+        <Interval display={'12mo'} interval={'pastYear'} onClick={setInterval} />
       </ul>
     </div>
   );
