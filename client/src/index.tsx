48a06a51b78fcc6e72e50510324e232463c9cedc
agaricide <agaricide@gmail.com>
2019-02-13 21:04:03 -0600

    Small tweak to index.ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,9 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import * as parallax from './parallax';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+parallax.addEffect(document.getElementById('background'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-parallax.addEffect(document.getElementById('background'));
+
