43b5c17f8495f2cc7422a79d6b0cf9ba2cc36443
agaricide <agaricide@gmail.com>
2019-02-16 17:40:09 -0600

    Clearing git cache to stop this annoying build/server.js issue

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import * as fastify from 'fastify';
+import * as helmet from 'fastify-helmet';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import * as RainfallReport from './pollReport';
+RainfallReport.pollReport();
+
+const helmetConfig: helmet.FastifyHelmetOptions = {
+  hidePoweredBy: {
+    setTo: 'AGAR'
+  }
+};
+
+const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({});
+
+server.register(helmet, helmetConfig);
+
+server.get('/api/report', (_, response) => {
+  response.code(200).send(RainfallReport.cached);
+});
+
+server.listen(8081, (error, address) => {
+  if (error) throw error;
+  console.log(`Server listening on port ${address}`);
+});
