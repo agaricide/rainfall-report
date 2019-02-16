39bd25f93c460f4902302769554c302f5bb3bb5e
agaricide <agaricide@gmail.com>
2019-02-16 16:51:12 -0600

    Cleaner seperation of concerns and security middleware in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,25 @@
 import * as fastify from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from 'http';
+import * as helmet from 'fastify-helmet';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import * as Rainfall from './pollRainfall';
+
+Rainfall.pollRainfall();
+
+const helmetConfig: helmet.FastifyHelmetOptions = {
+  hidePoweredBy: {
+    setTo: 'AGAR'
+  }
+};
+
 const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({});
-import axios from 'axios';
 
-let cached = {};
+server.register(helmet, helmetConfig);
 
-const interval = 5 * 60 * 1000;
-
-const fetchRainfallReport = async () => {
-    const result = await axios('https://www.dallasopendata.com/resource/xm9r-xkdu.json');
-    cached = result.data;
-}
-
-fetchRainfallReport();
-
-setInterval(() => {
-    fetchRainfallReport();
-}, interval);
-
-server.get('/api/report', (request, reply) => {
-    // for better reply types, see: https://bit.ly/2E45Rzy
-    reply.code(200).send(cached);
+server.get('/api/report', (_, response) => {
+  response.code(200).send(Rainfall.cached);
 });
 
-server.listen(8081, (err, address) => {
-    if (err) throw err;
-    console.log("server listening on " + address);
+server.listen(8081, (error, address) => {
+  if (error) throw error;
+  console.log(`Server listening on port ${address}`);
 });
