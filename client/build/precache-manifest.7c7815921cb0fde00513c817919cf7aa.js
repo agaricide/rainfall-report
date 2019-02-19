3d7538f3f206ba48d34043fa10af4da1cd2c5b56
agaricide <agaricide@gmail.com>
2019-02-19 02:15:46 -0600

    Adding client/build to git because pi speed is prohibitive

---
+++ client/build/precache-manifest.7c7815921cb0fde00513c817919cf7aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b9ad43902fc8e4f8f3d184824a02d77",
+    "url": "/static/media/gps-3.6b9ad439.svg"
+  },
+  {
+    "revision": "6e4eb28d1c3c1cc8956d867e628dacc8",
+    "url": "/static/media/gps-2.6e4eb28d.svg"
+  },
+  {
+    "revision": "219edb91c222134fb56e1a67c30bf1ba",
+    "url": "/static/media/gps-1.219edb91.svg"
+  },
+  {
+    "revision": "d0b0ecec47a456e5d7486e7d4ab02d28",
+    "url": "/static/media/agar.d0b0ecec.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25f01511fd118ca031e4",
+    "url": "/static/js/main.25f01511.chunk.js"
+  },
+  {
+    "revision": "6dcd8f2d04bc47740e1d",
+    "url": "/static/js/1.6dcd8f2d.chunk.js"
+  },
+  {
+    "revision": "25f01511fd118ca031e4",
+    "url": "/static/css/main.d23f489c.chunk.css"
+  },
+  {
+    "revision": "5e29303ed5e31bd0f281c8f9a2ccb829",
+    "url": "/index.html"
+  }
+];
